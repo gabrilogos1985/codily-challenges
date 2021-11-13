5cf50934ed436a07c1576ce0745c3c47d50b883e
Gabo <jose.paz@tcpip.tech>
2021-11-13 11:02:07 -0600

    performing edge cases, need review

---
+++ src/test/java/BinarytTreeTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class BinarytTreeTest {
+
+
+    @Test
+    public void baseTest() {
+//        int[] numbers = {1, 2, 3}; //1,3 = 2,  1,2,3 = 2, 2 = 2,
+//        int s = 2;
+//        Assertions.assertEquals(3, countArithmeticMeanOccurrences(numbers, s));
+//        Assertions.assertEquals(3, countArithmeticMeanOccurrences(new int[]{5,3,6,2}, 4));
+        Assertions.assertEquals(51, countArithmeticMeanOccurrences(IntStream.range(1, 10).toArray(), 5));
+//        Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 50).toArray(), 5));
+//       Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 20).map(v-> new Random().nextInt(8)).toArray(), 5));
+//        Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 10_050).toArray(), 10000));
+        //countArithmeticMeanOccurrences(IntStream.range(1, 100_050).toArray(), 100_000);
+    }
+
+    private int countArithmeticMeanOccurrences(final int[] arr, final int s) {
+
+        int r =  arr.length;
+        int n = arr.length;
+
+
+
+//        AtomicInteger allArithNumbers = new AtomicInteger(0);
+        //for (int i = 0; i < numbers.length; i++) {
+          //  LinkedList<Integer> baseNumbers = new LinkedList<>(Arrays.asList(numbers[i]));
+            //countIfArithNumber(baseNumbers, allArithNumbers, s);
+            //calculateRecursive(baseNumbers, getTail(numbers, i + 1), allArithNumbers, s);
+//        Arrays.sort(numbers);
+//            calculateRecursive(new LinkedList<>(), numbers, allArithNumbers, s);
+        //}
+        return returnSubsequenceCount(arr,s);
+    }
+
+
+    public static int returnSubsequenceCount(int[] X, int S) {
+        int counter = 0;
+
+        for (int i = 0; i < X.length; i++) {
+            int[] dpSum = new int[X.length];
+            Map<Integer, List<Integer>> dpNums = new HashMap<>();
+
+            dpSum[i] = X[i];
+            dpNums.put(i, new LinkedList<>(Arrays.asList(X[i])) );
+
+            if (X[i] == S) {
+                System.out.println("Solution:  " + X[i]);
+                counter++;
+            }
+
+            for (int j = i + 1; j < X.length; j++) {
+                int sum = dpSum[j - 1] + X[j];
+
+                List<Integer> integers = dpNums.get(j-1);
+                integers = integers == null ? new LinkedList<>() : integers;
+                integers.add(X[j]);
+                dpNums.put(j, integers);
+
+                dpSum[j] = sum;
+
+                if ((double) sum / (j - i + 1) == S) {
+                    System.out.println("Solution:  " + integers);
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
+
+
--- src/test/java/MeanAritmeticTest.java
+++ src/test/java/MeanAritmeticTest.java
@@ -1,43 +1,84 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class MeanAritmeticTest {
 
-
+        static int lastfirstnumber = 0;
     @Test
     public void baseTest() {
-
-        int[] numbers = {1, 2, 3, 4, 5};
-        int s = 3;
-        AtomicInteger allArithNumbers = new AtomicInteger(0);
-        for (int i = 0; i < numbers.length; i++) {
-            countIfArithNumber(List.of(numbers[i]), allArithNumbers, s);
-        }
-        allArithNumbers.get();
+        int[] numbers = {1, 2, 3}; //1,3 = 2,  1,2,3 = 2, 2 = 2,
+        int s = 2;
+       // Assertions.assertEquals(3, countArithmeticMeanOccurrences(numbers, s));
+      //  Assertions.assertEquals(3, countArithmeticMeanOccurrences(new int[]{5,3,6,2}, 4));
+    //    Assertions.assertEquals(51, countArithmeticMeanOccurrences(IntStream.range(1, 10).toArray(), 5));
+    //    Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 50).toArray(), 5));
+        //Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 20).map(v-> new Random().nextInt(8)).toArray(), 5));
+        Assertions.assertEquals(117, countArithmeticMeanOccurrences(IntStream.range(1, 10_050).toArray(), 500));
     }
 
-    private void calculateRecursive(int number, int[] others, AtomicInteger allArithNumbers, int expected) {
-        if (others.length > 0) {
+    private int countArithmeticMeanOccurrences(final int[] numbers, final int s) {
+        AtomicInteger allArithNumbers = new AtomicInteger(0);
+        //for (int i = 0; i < numbers.length; i++) {
+          //  LinkedList<Integer> baseNumbers = new LinkedList<>(Arrays.asList(numbers[i]));
+            //countIfArithNumber(baseNumbers, allArithNumbers, s);
+            //calculateRecursive(baseNumbers, getTail(numbers, i + 1), allArithNumbers, s);
+        Arrays.sort(numbers);
+            calculateRecursive(new LinkedList<>(), numbers, allArithNumbers, s);
+        //}
+        return allArithNumbers.get();
+    }
+
+    private void calculateRecursive(List<Integer> baseNumbers, int[] others, AtomicInteger allArithNumbers, int expected) {
+        if(!baseNumbers.isEmpty() && lastfirstnumber != baseNumbers.get(0)) {
+            lastfirstnumber = baseNumbers.get(0);
+            System.out.println("LAST NUMBER: " + lastfirstnumber);
+        }
+        Integer reduce = baseNumbers.stream().reduce(0, (a, b) -> a + b);
+        if(reduce %100 == 0 && !baseNumbers.isEmpty()) System.out.println( "Sum " + reduce + " " + baseNumbers.size() + " " + baseNumbers.get(0));
+        if (others.length < 1 || calculateArithMean(baseNumbers) > expected) {
             return;
         }
         for (int i = 0; i < others.length; i++) {
-            countIfArithNumber(List.of(number, others[i]), allArithNumbers, expected);
-            //TODO: improve copy
-            calculateRecursive(others[i], Arrays.copyOfRange(others, 1, others.length), allArithNumbers,expected);
+            var currentNumbers = new LinkedList<>(baseNumbers);
+            currentNumbers.add(others[i]);
+            countIfArithNumber(currentNumbers, allArithNumbers, expected);
+            //System.out.println("Recursive: " + currentNumbers);
+
+            if(i < others.length - 1) {
+                var accumulatorList = new LinkedList<>(baseNumbers);
+                accumulatorList.add(others[i]);
+             calculateRecursive(accumulatorList, getTail(others, i + 1), allArithNumbers, expected);
+            }
         }
     }
 
-    private void countIfArithNumber(final List<Integer> numbers, final AtomicInteger allArithNumbers, int expected) {
-        if (calculateArithMean(numbers) == expected) {
+    private int[] getTail(final int[] others, int fromIndex) {
+        if(others.length > fromIndex) {
+            //TODO: improve copy
+            return Arrays.copyOfRange(others, fromIndex, others.length);
+        }
+        return new int[0];
+    }
+
+    private void countIfArithNumber(final List<Integer> numbers, final AtomicInteger allArithNumbers, double expected) {
+        double arithMean = calculateArithMean(numbers);
+        boolean comparisom = arithMean == expected;
+        if(arithMean %50 == 0) System.out.println("Mean " + arithMean);
+        if (comparisom) {
+            System.out.printf("Combinacion %s, %s == %s => %s \n", numbers, arithMean, expected, comparisom);
             allArithNumbers.incrementAndGet();
         }
     }
 
-    private int calculateArithMean(List<Integer> numbers) {
-        var sum = numbers.stream().reduce(0, (a, b) -> a + b);
+    private double calculateArithMean(List<Integer> numbers) {
+        double sum = numbers.stream().reduce(0, (a, b) -> a + b);
         return sum / numbers.size();
     }
 }
