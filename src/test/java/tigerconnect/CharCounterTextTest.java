04018edc3bb8d82f53b86dad072b95ee867c1914
Gabo <jose.paz@tcpip.tech>
2021-12-05 09:24:35 -0600

    resolve counting chars with REgex

--- src/test/java/tigerconnect/CharCounterTextTest.java
+++ src/test/java/tigerconnect/CharCounterTextTest.java
@@ -3,19 +3,25 @@ package tigerconnect;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CharCounterText {
+class CharCounterTextTest {
 
 
     @Test
-    public void testCharCounterText() {
+    void testCharCounterText() {
         assertEquals("l1", count("l"));
         assertEquals("l1m1", count("lm"));
         assertEquals("l1m1n3t2", count("lmnnntt"));
         assertEquals("l1m2n3t2l4", count("lmmnnnttllll"));
         assertEquals("l5m2n3t2l4", count("lllllmmnnnttllll"));
+        assertEquals("l5m2n3t2l4c1", count("lllllmmnnnttllllc"));
+        assertEquals("l5 2m2n3 1t2l4 3c1", count("lllll  mmnnn ttllll   c"));
+        assertEquals("l5 1m2n3 2t2l4 3c1 1", count("lllll mmnnn  ttllll   c "));
+        assertEquals("", count(""));
     }
 
     private String countStream(final String text) {
@@ -43,8 +49,14 @@ public class CharCounterText {
         return sb.toString();
     }
 
-    //countWithFor
+
     private String count(final String text) {
+        return Pattern.compile("(.)\\1*").matcher(text).results()
+                .map(m -> m.group(1) + m.group().length())
+                .collect(Collectors.joining());
+    }
+
+    private String countUsingFor(final String text) {
         StringBuilder counterText = new StringBuilder();
         int counter = 0;
         for (int index = 0; index < text.length(); index++) {
